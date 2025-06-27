5d8968501a91e6e620dfc4a0b614a815fc7ed463
Sanjay Kumar <sanjayvkumar31@gmail.com>
2025-06-27 17:09:09 -0400

    migrated to FastAPI

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
-from flask import Flask
+from fastapi import FastAPI
 from mta import getMTA_realtime
 from weather import get_weather
-app = Flask(__name__)
 
-@app.route("/api/mta")
+app = FastAPI()
+
+@app.get("/api/mta")
 def mta_update():
     return getMTA_realtime()
 
-@app.route("/api/weather")
+@app.get("/api/weather")
 def weather_update():
     return get_weather()
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001,debug=True)
